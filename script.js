71847b5ba3edf0372fd4ccd2d7a87b0035e5397e
adarshsrajesh <adarshsrajesh@gmail.com>
2025-05-14 14:56:47 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const socket = io("http://192.168.137.69:5000");
-const socket = io("http://localhost:5000");
+const socket = io("https://audio-call-sfu-server.onrender.com/:5000");
 
 const peers = {};
 let localStream;
